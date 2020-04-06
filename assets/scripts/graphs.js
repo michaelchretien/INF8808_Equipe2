1a76257f919cf545634cca26743e5310c9c6c78b
Michael Chretien <michael.chretien@polymtl.ca>
2020-04-05 20:33:39 -0400

    Version fonctionnelle de la viz 4 (Reste à arranger ui)

--- assets/scripts/graphs.js
+++ assets/scripts/graphs.js
@@ -22,10 +22,7 @@ class GraphViz {
 
         this.x = d3.scaleTime()
             .range([0, rect.width]);
-
-        this.yGraph1 = d3.scaleLinear()
-            .range([0, rect.height / 2]);
-
+        this.xAxis = d3.axisBottom(this.x).tickFormat(localization.getFormattedDate);
 
         //this.line = this.createLine(this.x, this.y);
         //this.xAxis = d3.axisBottom(this.x)//.tickFormat(localization.getFormattedDate);
@@ -50,47 +47,117 @@ class GraphViz {
     }
 
     update(newDomain) {
-        this._updatePeriods(newDomain)
-        this._updateGraph1(newDomain)
-        this._updateGraph2(newDomain)
+        // Mettre le domaine des x à jour
+        this.x.domain(d3.event.selection === null ? newDomain.domain() : d3.event.selection.map(newDomain.invert));
+
+        this._updatePeriods()
+        this._updateGraph1()
+        this._updateGraph2()
     }
 
     _addBackground() {
         this.g.append("rect")
             .attr("width", this.rect.width)
             .attr("height", this.rect.height)
-            .style("stroke", "black")
+            .style("stroke", "beige")
             .style("fill", "none")
             .style("stroke-width", 1)
     }
+
     _addGraph1(crashes) {
-        var contextLineGroups = this.g.append("g")
-            .attr("class", "context")
-            .selectAll("g")
-            .data(sources)
-            .enter().append("g")
+        var g = this.g.append("g")
+            .attr("transform", "translate( 0, " + (this.rect.height / 2) + ")");
 
-        contextLineGroups.append("path")
-            .attr("class", "line")
-            .attr("d", d => this.line(d.values))
-            .attr("clip-path", "url(#graphviz_clip)")
-            .style("stroke", d => (d.name === "Moyenne") ? "black" : color(d.name))
-            .style("stroke-width", d => (d.name === "Moyenne") ? 2 : 1)
-            .attr("id", d => "context" + d.name);
+        var height = this.rect.height / 2
+        this.yGraph1 = d3.scaleLinear()
+            .domain([0, 600])
+            .range([height, 0]);
 
-        this.g.append("g")
+        var x = this.x
+        var y = this.yGraph1
+
+        //this.xAxisGraph1 = d3.axisBottom(this.x).tickFormat(localization.getFormattedDate);
+        this.yAxisGraph1 = d3.axisLeft(this.yGraph1);
+
+        g.append("g")
             .attr("class", "x axis")
-            .attr("transform", "translate(0," + (this.rect.height - 30) + ")")
+            .attr("transform", "translate(0," + (this.rect.height / 2) + ")")
             .call(this.xAxis);
 
-        this.g.append("g")
+        g.append("g")
             .attr("class", "y axis")
-            .attr("transform", "translate(50,0 )")
-            .call(this.yAxis);
+            .attr("transform", "translate(0,0 )")
+            .call(this.yAxisGraph1);
+
+        g.selectAll("dot")
+            .data(crashes)
+            .enter()
+            .append("circle")
+            .attr("class", "graph1")
+            .attr("cx", d => x(d.Date))
+            .attr("cy", d => y(d.Fatalities))
+            .attr("clip-path", "url(#graphviz_clip)")
+            .attr("r", 1.5)
+            .style("fill", "black")
     }
 
     _addGraph2(crashes) {
+        var g = this.g.append("g");
 
+        var height = this.rect.height / 2
+        this.yGraph2 = d3.scaleLinear()
+            .domain([0, 600])
+            .range([height, 0]);
+
+        var x = this.x
+        var y = this.yGraph2
+
+        //this.xAxisGraph1 = d3.axisBottom(this.x).tickFormat(localization.getFormattedDate);
+        this.yAxisGraph2 = d3.axisLeft(this.yGraph2);
+
+        g.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(this.xAxis);
+
+        g.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(0,0 )")
+            .call(this.yAxisGraph2);
+
+        g.selectAll("dot")
+            .data(crashes)
+            .enter()
+            .append("circle")
+            .attr("class", "graph2")
+            .attr("cx", d => x(d.Date))
+            .attr("cy", d => y(d.Aboard))
+            .attr("clip-path", "url(#graphviz_clip)")
+            .attr("r", 1.5)
+            .style("fill", "black")
+        /*var contextLineGroups = this.g.append("g")
+                  .attr("class", "context")
+                  .selectAll("g")
+                  .data(sources)
+                  .enter().append("g")
+      
+              contextLineGroups.append("path")
+                  .attr("class", "line")
+                  .attr("d", d => this.line(d.values))
+                  .attr("clip-path", "url(#graphviz_clip)")
+                  .style("stroke", d => (d.name === "Moyenne") ? "black" : color(d.name))
+                  .style("stroke-width", d => (d.name === "Moyenne") ? 2 : 1)
+                  .attr("id", d => "context" + d.name);
+      
+              this.g.append("g")
+                  .attr("class", "x axis")
+                  .attr("transform", "translate(0," + (this.rect.height - 30) + ")")
+                  .call(this.xAxis);
+      
+              this.g.append("g")
+                  .attr("class", "y axis")
+                  .attr("transform", "translate(50,0 )")
+                  .call(this.yAxis);*/
     }
 
     _addPeriods(periods) {
@@ -106,7 +173,7 @@ class GraphViz {
             .attr("width", d => x(d.EndDate) - x(d.StartDate))
             .attr("height", height)
             .attr("clip-path", "url(#graphviz_clip)")
-            .attr("fill", d => "rgba(51, 204, 255, 128");
+            .attr("fill", d => "rgba(128, 128, 128, 0.2");
 
         // Texte
         this.g.append("g").selectAll(".periodName")
@@ -117,8 +184,9 @@ class GraphViz {
             .attr("class", "periodName")
             .attr("id", function (d) { return "periodName" + d.Name; })
             .attr("text-anchor", "middle")
+            .attr("clip-path", "url(#graphviz_clip)")
             .attr("x", d => x(this._getMiddleDate(d)))
-            .attr("y", function (d) { return height - 4 })
+            .attr("y", function (d) { return 0 })
             .attr("dy", "1.5em")
             .attr("font-weight", "bold");
     }
@@ -138,7 +206,23 @@ class GraphViz {
             .curve(d3.curveBasisOpen);
     }
 
-    _updateGraph1(newDomain) {
+    _updateGraph1() {
+        var x = this.x
+        var y = this.yGraph1
+        this.g.selectAll(".graph1")
+            .attr("cx", d => x(d.Date))
+            .attr("cy", d => y(d.Fatalities))
+        this.g.select(".x.axis").call(this.xAxis);
+    }
+
+
+    _updateGraph2() {
+        var x = this.x
+        var y = this.yGraph1
+        this.g.selectAll(".graph2")
+            .attr("cx", d => x(d.Date))
+            .attr("cy", d => y(d.Aboard))
+        this.g.select(".x.axis").call(this.xAxis);
         // TODO: Redessiner le graphique focus en fonction de la zone sélectionnée dans le graphique contexte.
         /*var line = this.line
         this.x.domain(d3.event.selection === null ? newDomain.domain() : d3.event.selection.map(newDomain.invert));
@@ -150,14 +234,8 @@ class GraphViz {
     }
 
 
-    _updateGraph2(newDomain) {
-
-    }
-
-
-    _updatePeriods(newDomain) {
+    _updatePeriods() {
         var x = this.x
-        this.x.domain(d3.event.selection === null ? newDomain.domain() : d3.event.selection.map(newDomain.invert));
         this.g.selectAll("rect.period")
             .attr("x", d => x(d.StartDate))
             .attr("width", d => x(d.EndDate) - x(d.StartDate));
